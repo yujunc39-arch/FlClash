529ef3a5626af0e4f6b247bcafe0f44012517052
yujunc39-arch <yujunc39@gmail.com>
2026-06-06 16:53:48 +0800

    Add files via upload

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -25,7 +25,7 @@ val isRelease =
 
 
 android {
-    namespace = "com.follow.clash"
+    namespace = "lin.jiu.ser"
     compileSdk = libs.versions.compileSdk.get().toInt()
     ndkVersion = libs.versions.ndkVersion.get()
 
@@ -37,7 +37,7 @@ android {
     }
 
     defaultConfig {
-        applicationId = "com.follow.clash"
+        applicationId = "lin.jiu.ser"
         minSdk = flutter.minSdkVersion
         targetSdk = libs.versions.targetSdk.get().toInt()
         versionCode = flutter.versionCode
